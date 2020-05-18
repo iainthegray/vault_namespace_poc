6df7915b1721d56c07cac758ceecd22f25dc103c
ianthegray <iain.dv@gmail.com>
2020-05-18 08:08:44 +0100

    added internal groups and policies

--- main.tf
+++ main.tf
@@ -16,6 +16,7 @@ provider "vault" {
   address = var.vault_addr
   alias = "my_namespace"
   namespace = var.namespace_name
+  token = var.token
 }
 /* Authentication backends */
 
@@ -31,11 +32,31 @@ resource "vault_auth_backend" "userpass" {
 }
 
 /* Secrets backends */
-
 resource "vault_mount" "kv" {
   count = (var.use_kv ? 1 : 0)
   depends_on  = [vault_namespace.my_namespace]
   provider    = vault.my_namespace
   path        = var.kv_path
   type        = "kv"
+}
+
+/*Policy Creation
+This is based on the use_policy variable */
+
+resource "vault_policy" "ns_policies" {
+  provider    = vault.my_namespace
+  for_each = var.policies
+  name   = each.key
+  policy = each.value
+}
+
+/* Namespace LDAP
+This is set up based on the use_ldap variable */
+resource "vault_identity_group" "internal" {
+  count = (var.use_ldap ? 1 : 0)
+  provider    = vault.my_namespace
+  name     = var.ns_group_name
+  type     = "internal"
+  member_group_ids = [var.ext_group_id]
+  policies = keys(var.policies)
 }
--- variables.tf
+++ variables.tf
@@ -1,6 +1,7 @@
 variable "vault_addr" {
   description = "The address of the Vault server"
   type = string
+  default = "http://127.0.0.1:8200"
 }
 
 variable "token" {
@@ -12,12 +13,25 @@ variable "namespace_name" {
   description = "The Vault namespace to be created"
   type = string
 }
-
+/* This section is the set of switches that determine which parts
+of he module are used */
 variable "use_userpass" {
   description = "a variable set to true or false to determine if a userpass auth method should be created"
   default = false
 }
-
+variable "use_kv" {
+  description = "a variable set to true or false to determine if a kv secrets method should be created"
+  default = false
+}
+variable "use_policy" {
+  description = "a variable set to true or false to determine if policies should be created"
+  default = false
+}
+variable "use_ldap" {
+  description = "a variable set to true or false to determine if ldap groups should be created"
+  default = false
+}
+/* Options that have sensible defaults. These are linked to the switches above */
 variable "up_path" {
   description = "The path of the userpass auth engine"
   type = string
@@ -29,13 +43,25 @@ variable "up_lease_ttl" {
   default = "600m"
 }
 
-variable "use_kv" {
-  description = "a variable set to true or false to determine if a kv secrets method should be created"
-  default = false
-}
-
 variable "kv_path" {
   description = "The path of the kv secrets engine"
   type = string
   default = "kv"
+}
+variable "policies" {
+  description = "A map of policies to create in the namespace"
+  type = map
+  default = {
+  }
+}
+/* LDAP Variables */
+variable "ns_group_name" {
+  description = "The name the LDAP group will be added to"
+  type = string
+  default = ""
+}
+variable "ext_group_id" {
+  description = "The id of the external group to be added to the namespace group"
+  type = string
+  default = ""
 }
